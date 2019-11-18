d248944c30904396c7ab4c6a2f181b812cb3d639
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-18 11:09:10 -0500

    madd content to Navbar

--- starter-code/src/components/NavBar.jsx
+++ starter-code/src/components/NavBar.jsx
@@ -3,6 +3,24 @@ import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <header>
+        <h1>React IronBeers</h1>
+        <ul className={"nav nav-pills nav-fill justify-content-center"}>
+          <li className="nav-item">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/all-beers"}>All beers</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/random-beer"}>Random Beer</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/new-beer"}>New Beer</Link>
+          </li>
+        </ul>
+      </header>
+    );
   }
 }
