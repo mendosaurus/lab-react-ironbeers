7ace475e272c32acd889d82e78e7ee83c5e92459
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-18 10:44:31 -0500

    make Home page

--- starter-code/src/components/Card.jsx
+++ starter-code/src/components/Card.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Card extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Link to={this.props.link}>
+        <div className="card">
+          <img src={this.props.image} alt="" className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.title}</h5>
+            <p className="card-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque,
+              aspernatur? Mollitia, officia odit maxime temporibus cum quasi
+              enim totam debitis.
+            </p>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 }
--- starter-code/src/components/Home.jsx
+++ starter-code/src/components/Home.jsx
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Card from "./Card";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Card
+          title={"All beers"}
+          image={"./images/beers.png"}
+          link={"/all-beers"}
+        />
+        <Card
+          title={"Random beer"}
+          image={"./images/random-beer.png"}
+          link={"/random-beer"}
+        />
+        <Card
+          title={"New beer"}
+          image={"./images/new-beer.png"}
+          link={"/new-beer"}
+        />
+      </div>
+    );
+  }
 }
