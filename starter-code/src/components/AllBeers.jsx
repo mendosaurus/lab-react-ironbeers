fd455dfa025852743602196a2deb31040ece6361
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-18 11:59:55 -0500

    display all beers in /all-beers

--- starter-code/src/components/AllBeers.jsx
+++ starter-code/src/components/AllBeers.jsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
-import Beer from './Beer'
+import Beer from "./Beer";
 
 export default class AllBeers extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      allBeers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers").then(response => {
+      this.setState({ allBeers: response.data });
+    });
+  }
+
+  render() {
+    console.log(this.state.allBeers);
+    return (
+      <div>
+        {this.state.allBeers.map((beer, i) => {
+          return <Beer beer={beer} key={i} />;
+        })}
+      </div>
+    );
+  }
 }
--- starter-code/src/components/Beer.jsx
+++ starter-code/src/components/Beer.jsx
@@ -1,12 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Beer extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img
+              src={this.props.beer.image_url}
+              className="card-img"
+              alt="..."
+              width="20px"
+              height="100px"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{this.props.beer.name}</h5>
+              <p className="card-text">{this.props.beer.tagline}</p>
+              <p className="card-text">
+                <small className="text-muted">
+                  {this.props.beer.contributed_by}
+                </small>
+              </p>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+
+      //   <div className='card'>
+      //     <div>
+      //       <img src={this.props.beer.image_url} alt="" />
+      //     </div>
+      //     <div>
+      //       <h5>{this.props.beer.name}</h5>
+      //       <p>{this.props.beer.tagline}</p>
+      //       <sub>{this.props.beer.contributed_by}</sub>
+      //     </div>
+      //   </div>
+    );
+  }
 }
